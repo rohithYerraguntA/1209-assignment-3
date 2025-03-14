349cda3727e9aa28f9b6de3595c08f771999d60f
rohith_yerragunta <rohith.yerragunta@dcmail.ca>
2025-03-14 18:52:49 -0400

    Update test_funcs.py

--- tests/mymodule/test_funcs.py
+++ tests/mymodule/test_funcs.py
@@ -6,7 +6,7 @@ from myapp.mymodule.funcs import *
 @pytest.mark.easy_operation
 def test_add():
     # This test will fail.
-    assert add(4, 8) == 14
+    assert add(4, 8) == 12
 
 @pytest.mark.easy_operation
 def test_subtract():
@@ -19,3 +19,8 @@ def test_multiply():
 @pytest.mark.difficult_operation
 def test_divide():
     assert divide(56, 8) == 7
+
+@pytest.mark.square_test
+def test_square_area():
+    assert square_area(71) == 16  # Using last two digits of my student ID - 100936904
+    
